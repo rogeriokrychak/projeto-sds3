064d76084841133a3c53d57c970cda68e8b60336
rogeriokrychak <roger.krychak@gmail.com>
2021-05-10 16:05:50 -0300

    Modify Env var

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api=axios.create({
-    baseURL:'https:https://rogerio-aulas-sds3.herokuapp.com/'?? 'http://localhost:8080';
+    baseURL:'https:https://rogerio-aulas-sds3.herokuapp.com/'?? 'http://localhost:8080'
 });
