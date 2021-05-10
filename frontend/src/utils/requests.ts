6643fa49a69245e4abf0983ce20ba8f954d3101e
rogeriokrychak <roger.krychak@gmail.com>
2021-05-10 16:26:24 -0300

    Modify Env var

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,5 +1,1 @@
-import axios from 'axios';
-
-export const api=axios.create({
-    baseURL:'https:https://rogerio-aulas-sds3.herokuapp.com/'?? 'http://localhost:8080'
-});
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'https://rogerio-aulas-sds3.herokuapp.com/';
