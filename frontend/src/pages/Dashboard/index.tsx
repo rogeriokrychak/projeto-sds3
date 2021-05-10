d38c983aaec1ead23755cba537834408eae7df8e
rogeriokrychak <roger.krychak@gmail.com>
2021-05-10 15:19:52 -0300

    Errors

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -10,7 +10,6 @@ const Dashboard = () => {
       <NavBar />
       <div className="container">
         <h1 className="text-primary py-3">Dashboard de Vendas</h1>
-
         <div className="row px=3">
           <div className="col-sm-6">
             <h5 className="text-center text-secondary">Taxa de Sucesso (%)</h5>
